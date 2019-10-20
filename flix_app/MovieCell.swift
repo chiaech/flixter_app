f13776b74a4033ebe59e554897659d0062a96fc9
chia <38141245+chiaech@users.noreply.github.com>
2019-10-20 05:07:33 -0700

    Add files via upload

---
+++ flix_app/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  flix_app
+//
+//  Created by chia seed on 10/20/19.
+//  Copyright © 2019 Chia E. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
